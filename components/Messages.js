0a63ce3499e2eba4d08fd1b36b31e1214efa0fb0
rampency <deeps918@gmail.com>
2021-06-06 16:02:09 -0700

    2 files modified

--- components/Messages.js
+++ components/Messages.js
@@ -1,15 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import React from 'react';
-import messages from '../data.json';
+import data from '../data.json';
 import Message from './Message';
 import Button from './Button';
 function Messages() {
-  const [messageList, setMessageList] = useState(messages['messages']);
+  const [messageList, setMessageList] = useState(data['messages']);
   const [accending, setAccending] = useState(false);
+  useEffect(() => {
+    console.log('mounted');
+    let foo = messageList
+      .slice()
+      .filter(
+        (message, index, self) =>
+          index ===
+          self.findIndex(
+            t => t.uuid === message.uuid && t.content === message.content
+          )
+      );
+    setMessageList(foo);
+    return () => console.log('unmounting...');
+  }, []); // <-- add this empty array here
+
   let changeMessageOrder = () => {
     setAccending(!accending);
     setMessageList([]);
-
     let sorted = messageList.sort((d1, d2) => {
       return accending
         ? new Date(d1.sentAt).getTime() - new Date(d2.sentAt).getTime()
@@ -18,15 +32,17 @@ function Messages() {
     setMessageList(sorted);
   };
   const onDelete = id => {
-    console.log('delete' + id);
     setMessageList(messageList.filter(message => message.uuid !== id));
+    console.log(messageList.length);
   };
   return (
     <div className="container">
       <Button toggle={accending} onClick={changeMessageOrder} />
-      {messageList.map(message => (
-        <Message key={message.uuid} message={message} onDelete={onDelete} />
-      ))}
+      {messageList.map((message, index) => {
+        if (index < 5) {
+          return <Message message={message} onDelete={onDelete} />;
+        }
+      })}
     </div>
   );
 }
